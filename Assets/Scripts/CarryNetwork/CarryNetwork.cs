6c77737734cf51bd20ac793ebfadb3446b85adc8
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2022-05-10 04:42:32 +0300

    Implemented route by node name for the item carrying algorithm

--- Assets/Scripts/CarryNetwork/CarryNetwork.cs
+++ Assets/Scripts/CarryNetwork/CarryNetwork.cs
@@ -5,21 +5,45 @@ using UnityEngine;
 public class CarryNetwork : MonoBehaviour
 {
     CarryNode[] Nodes;
+    Dictionary<string, CarryNode> NodeDictionary;
 
     void Start()
     {
         Nodes = gameObject.GetComponentsInChildren<CarryNode>();
 
+        NodeDictionary = new Dictionary<string, CarryNode>();
+        foreach (CarryNode node in Nodes)
+        {
+            NodeDictionary.Add(node.name, node);
+        }
+
         // TEST AREA
-        Vector2[] route = FindRoute(-25, 25, Nodes[0]);
-        foreach (Vector2 vector in route)
+        Vector2[] route1 = FindRouteToSite(-25, 25);
+        foreach (Vector2 vector in route1)
+        {
+            Debug.Log(vector);
+        }
+        Vector2[] route2 = FindRouteByTargetName(-25, 25, "CarryNode Pointless Side 1");
+        foreach (Vector2 vector in route2)
         {
             Debug.Log(vector);
         }
         // TEST AREA
     }
 
-    // TODO Find route by name (dictionary näille), Find route to start (pohjaa by nameen)
+    public Vector2[] FindRouteToSite(float startX, float startZ)
+    {
+        return FindRouteByTargetName(startX, startZ, "CarryNode Site");
+    }
+
+    public Vector2[] FindRouteByTargetName(float startX, float startZ, string name)
+    {
+        if (NodeDictionary.TryGetValue(name, out CarryNode node))
+        {
+            return FindRoute(startX, startZ, node);
+        }
+        throw new System.Exception("No CarryNode with name " + name + " was found.");
+    }
 
     public Vector2[] FindRoute(float startX, float startZ, CarryNode targetNode)
     // Returns a list of all nodes on the route including the first one that needs to be reached and the target node
@@ -47,6 +71,7 @@ public class CarryNetwork : MonoBehaviour
     // Returns the entire route (including the first node), NOTE: I'm not completely sure that A* is working, but I have not been able to show that it is not.
     {
         C5.IntervalHeap<CarryNodeState> stateQueue = new C5.IntervalHeap<CarryNodeState>(new CarryNodeStateComparer(targetNode.XZVector));
+        // Using an external library (C5) for priority queue
         ISet<Vector2> vectorsVisited = new HashSet<Vector2>();
 
         stateQueue.Add(new CarryNodeState(startNode, startCords));
